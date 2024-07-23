3c30b1ff3db1cd70e9daa56645fac954eab73446
preye <omusukup@yahoo.com>
2024-07-23 15:58:08 +0100

    style(fonts): add fonts style

--- src/components/sections/Showcase.tsx
+++ src/components/sections/Showcase.tsx
@@ -4,6 +4,7 @@ import TransparentBtn from "../ui/btns/TransparentBtn";
 import ShowcaseCard from "../ui/cards/ShowcaseCard";
 import Content from "../ui/Headers.tsx/Content";
 import MainTitle from "../ui/Headers.tsx/MainTitle";
+import SmallBtn from "../ui/btns/SmallBtn";
 
 const Showcase = () => {
   return (
@@ -13,21 +14,21 @@ const Showcase = () => {
           title="To become a thought leader faster, you need to look like one."
           className=" lg:text-4xl text-3xl leading-[37.2px] font-bold"
         />
-        <div className="lg:text-lg font-normal text-sm lg:leading-6 leading-[22.33px] flex flex-col gap-7">
+        <div className="lg:text-lg font-normal text-sm lg:leading-6 leading-[22.33px] flex flex-col gap-7 justify-center">
           <Content
-            title="Imagine your profile becomes a client magnet, radiating your authority and showcasing the life-changing transformations you deliver. Imagine you captivate your audience with compelling content that speaks directly to their deepest challenges and desires. "
+            title="Imagine your profile becomes a client magnet, radiating your authority and showcasing the life-changing transformations you deliver. Imagine you captivate your audience with compelling content that speaks directly to their deepest challenges and desires. Imagine dream clients reach out to you, ready to invest in your premium services because they see you as the go-to expert in your niche."
             className=""
           />
-          <Content
-            title="Imagine dream clients reach out to you, ready to invest in your premium services because they see you as the go-to expert in your niche.."
+          {/* <Content
+            title="Imagine dream clients reach out to you, ready to invest in your premium services because they see you as the go-to expert in your niche."
             className=""
-          />
+          /> */}
           <Content
             title="The Thought Leader Accelerator™ is your fast-track to achieving exactly that."
             className=" font-bold lg:text-2xl text-xl"
           />
         </div>
-        <div className="flex lg:gap-3 gap-1 scale-75 md:scale-100">
+        {/* <div className="flex lg:gap-3 gap-1 scale-75 md:scale-100">
           <FullBtn label="Book Free Call Now" />
           <Link to="/pricing">
             <TransparentBtn
@@ -35,11 +36,27 @@ const Showcase = () => {
               className="text-white text-lg font-medium border-[#0D7297] md:w-[208px] py-7 w-[161px]"
             />
           </Link>
-        </div>
+        </div> */}
       </div>
-      <div className="w-full mt-16">
+
+      <div className="w-full mt-16 flex flex-col justify-center items-center">
+        <MainTitle
+          title="My Recent Projects"
+          className=" lg:text-3xl text-2xl leading-[37.2px] font-bold"
+        />
         <ShowcaseCard />
       </div>
+      <a
+        href="https://www.behance.net/gallery/192388895/LinkedIn-Banner-Design"
+        target="_blank"
+        rel="noopener noreferrer"
+        className=" flex flex-col justify-center items-center"
+      >
+        <SmallBtn
+          label="View More"
+          className="text-xl font-medium cursor-pointer hover:to-color-end/80 "
+        />
+      </a>
     </section>
   );
 };
